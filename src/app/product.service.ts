54dc2bafa92c21637aa7f9c06cfb79bc7511f655
hernando alba <hernandoalba09@gmail.com>
2023-03-24 16:30:37 +0800

    updated

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,10 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
 import { Album } from './album';
 
 import { Observable } from 'rxjs/Observable';
 
 import { Product } from './product';
-import { Http } from '@angular/http';
-import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ProductService {
