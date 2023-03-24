1b06d669f4867e5a2532bfb27bb0355e7d1ea3f2
hernando alba <hernandoalba09@gmail.com>
2023-03-24 16:26:03 +0800

    updated

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,10 +1,5 @@
-import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
-
-import { Album } from './album';
-
-import { Observables } from 'rxjs/Observable'
 
 import { Product } from './product';
 
